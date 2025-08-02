77168ef996c49abaf88899ab9a32967e5f491fd3
qkrwlswo96 <qkrwlswo96@naver.com>
2025-08-02 15:39:37 +0900

    Todo card CSS Optimization

--- Todo-list/src/App.jsx
+++ Todo-list/src/App.jsx
@@ -5,17 +5,17 @@ import Todoeditor from './../component/Todoeditor';
 import TodoList from '../component/TodoList';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]); // 할일 목록 저장, todos=(할일 목록), setTodos=(할일 추가, 수정, 삭제)
 
   useEffect(() => {
-    const savedTodos = localStorage.getItem("todos");
+    const savedTodos = localStorage.getItem("todos"); // 웹페이지를 처음 열 때, localStorage에 저장된 할 일을 불러와서 todos에 넣음.
     if (savedTodos) {
       setTodos(JSON.parse(savedTodos));
     }
   }, []);
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos)); // 새로고침해도 todos의 변동 내용을 저장 
   }, [todos]);
 
   const handleCreate = (title, content) => {
@@ -28,19 +28,19 @@ const App = () => {
     setTodos([newTodo, ...todos]);
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = (id) => { // todo의 내용을 지우는 함수
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   };
 
-  const handleWorking = (id) => {
+  const handleWorking = (id) => { // 체크 시 완료 여부 알려줌
     const newTodos = todos.map((todo) =>
       todo.id === id? { ...todo, isDone: !todo.isDone} : todo
   );
   setTodos(newTodos);
 }
 
-const handleUpdate = (id, newtitle, newContent) => {
+const handleUpdate = (id, newtitle, newContent) => { // 제목 내용 수정
   setTodos(prev =>
     prev.map(todo =>
       todo.id === id ? { ...todo, title: newtitle, content: newContent } : todo
--- Todo-list/src/main.jsx
+++ Todo-list/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react' //HTML 연결
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react' // 경고성 연결, react 진단 도구
+import { createRoot } from 'react-dom/client' // react 최신기능 사용가능
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' // App 컴포넌트 불러오기
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( // 렌더링
   <StrictMode>
     <App />
   </StrictMode>,
